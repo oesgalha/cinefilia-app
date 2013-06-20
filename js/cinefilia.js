989bc202b4b498c467acffe8ae580c63ebed16f4
Lucas PD <lucasdg92@gmail.com>
2013-06-20 20:24:28 -0300

    adicionando paginas de detalhe de cinema e de filmes em cada cinema

--- js/cinefilia.js
+++ js/cinefilia.js
@@ -82,7 +82,6 @@ $(document).ready(function(){
       loadCinemas();
     },
     error: function(){
-      alert("nao");
       window.moviesData = JSON.parse(localStorage.moviesData);
       populatePosters();
       populateMoviesList();
@@ -106,7 +105,6 @@ $(document).ready(function(){
         localStorage.cinemasData = JSON.stringify(data);
       },
       error: function(){
-        alert("nao");
         window.cinemasData = JSON.parse(localStorage.cinemasData);
         populatePosters();
         populateMoviesList();
